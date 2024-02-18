11b945d59931be07664bdd9f9e1017c205b45c74
origin <https://github.com/jeromario/desafio_poo.git>
2024-02-18 19:49:59 -0300

    added files to attend avenger resource

---
+++ src/main/kotlin/dio/avengers/avengers/application/web/resource/request/AvengerRequest.kt
@@ -0,0 +1,37 @@
+package dio.avengers.avengers.application.web.resource
+
+
+import dio.avengers.avengers.domain.avenger.Avenger
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+
+data class AvengerRequest(
+    @field:NotNull(message = "Nick value requerid")
+    @field:NotBlank
+    @field:NotEmpty
+    val nick: String,
+    @field:NotNull(message = "Person value requerid")
+    @field:NotBlank
+    @field:NotEmpty
+    val person: String,
+    val description: String? = "",
+    val history: String? = ""
+){
+    fun toAvenger(): Avenger = Avenger(
+        nick = this.nick,
+        person = this.person,
+        description = this.description,
+        history = this.history
+    )
+    companion object{
+        fun to(id: Long?, request: AvengerRequest) = Avenger(
+            id = id,
+            nick = request.nick,
+            person = request.person,
+            description = request.description,
+            history = request.history
+        )
+    }
+
+}
